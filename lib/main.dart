144cede27b7ebde8c7456cc96dd0f32c3becc0bd
Muzammil Ali Khan <muzammil@gmail.com>
2021-06-28 23:45:02 +0500

    Todo App Practice

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:todo/todo.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Todo(),
+    );
+  }
+}
